67febf91fd0a5cd5101877b8751375f3486f7996
hebe-tian <linleil@126.com>
2022-05-14 21:13:09 +0800

    add wsgi.py

---
+++ wsgi.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+"""
+@File : wsgi.py
+@Time : 2022/5/14 21:11
+@Author : Linleil
+"""
+import os
+
+from dotenv import load_dotenv
+
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+import app
